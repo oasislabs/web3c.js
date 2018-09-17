63b76cc31992ac6e5a75aed9baf3b58bbe285bba
Will Scott <will@cypherpunk.email>
2018-09-17 09:38:32 -0700

    update to current webpack build command

--- demo/demo.js
+++ demo/demo.js
@@ -1,1 +1,16 @@
 // interactivity for the demo page.
+window.addEventListener('load', function() {
+  //getpublickey
+  var getpublickeyform = document.getElementById('getpublickey_form');
+  getpublickeyform.addEventListener('submit', onpublickeyform_submit);
+}, false);
+
+var onpublickeyform_submit = function (ev) {
+  var getpublickeyform = document.getElementById('getpublickey_form');
+  var addr = getpublickeyform.contract_address.value;
+
+  // Make call.
+
+  ev.preventDefault();
+  return false;
+};
