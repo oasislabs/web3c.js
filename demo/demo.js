9cadbfd6180c2fbb0c1aa2137cdcd2e23ded3bfa
Will Scott <will@cypherpunk.email>
2018-09-21 14:18:44 -0700

    add call form

--- demo/demo.js
+++ demo/demo.js
@@ -1,40 +1,60 @@
 // interactivity for the demo page.
-(function() {
-  window.addEventListener('load', function() {
-    //provider
-    getProvider();
+window.addEventListener('load', function() {
+  //provider
+  getProvider();
 
-    //getpublickey
-    let getpublickeyform = document.getElementById('getpublickey_form');
-    getpublickeyform.addEventListener('submit', onpublickeyform_submit);
-  }, false);
+  //getpublickey
+  let getpublickeyform = document.getElementById('getpublickey_form');
+  getpublickeyform.addEventListener('submit', onpublickeyform_submit);
+  //call
+  let callform = document.getElementById('call_enc_form');
+  callform.addEventListener('submit', oncallform_submit);
+}, false);
 
-  const getProvider = function () {
-    let ex = new Web3();
-    if (ex.currentProvider !== undefined) {
-      document.getElementById('provider').value = 'Browser Provided';
-      document.getElementById('provider').disabled = true;
-      return undefined;
-    } else {
-      return new Web3.providers.HttpProvider(document.getElementById('provider').value);
-    }
+const getProvider = function () {
+  let ex = new Web3();
+  if (ex.currentProvider !== undefined) {
+    document.getElementById('provider').value = 'Browser Provided';
+    document.getElementById('provider').disabled = true;
+    return undefined;
+  } else {
+    return new Web3.providers.HttpProvider(document.getElementById('provider').value);
+  }
+}
+
+const onpublickeyform_submit = function (ev) {
+  let getpublickeyform = document.getElementById('getpublickey_form');
+  let addr = getpublickeyform.contract_address.value;
+
+  // Make call.
+  let webc = new Web3c(getProvider());
+  try {
+    webc.confidential.getPublicKey(addr).then(function (resp) {
+      console.log(resp);
+    });
+  } catch (e) {
+    document.getElementById('getpublickey_result').innerHTML = e;
   }
 
-  const onpublickeyform_submit = function (ev) {
-    let getpublickeyform = document.getElementById('getpublickey_form');
-    let addr = getpublickeyform.contract_address.value;
+  ev.preventDefault();
+  return false;
+};
 
-    // Make call.
-    let webc = new Web3c(getProvider());
-    try {
-      webc.confidential.getPublicKey(addr).then(function (resp) {
-        console.log(resp);
-      });
-    } catch (e) {
-      document.getElementById('getpublickey_result').innerHTML = e;
-    }
+const oncallform_submit = function (ev) {
+  let callform = document.getElementById('call_enc_form');
+  let addr = callform.contract_address.value;
+  let data = callform.data.value;
 
-    ev.preventDefault();
-    return false;
-  };
-})();
+  // Make call.
+  let webc = new Web3c(getProvider());
+  try {
+    webc.confidential.call(addr, data).then(function (resp) {
+      console.log(resp);
+    });
+  } catch (e) {
+    document.getElementById('call_enc_result').innerHTML = e;
+  }
+
+  ev.preventDefault();
+  return false;
+};
