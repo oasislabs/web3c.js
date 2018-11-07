c4c1f17a5adda4ba495858f149262d06a14252f6
Will Scott <will@cypherpunk.email>
2018-11-07 02:58:30 -0800

    add some tests to key_manager, while we're at it

--- test/keymanager.js
+++ test/keymanager.js
@@ -17,4 +17,65 @@ describe('Key Manager', function() {
     let recover = await km2.decrypt(cyphertext);
     assert.equal('0x1234abcdef', recover);
   });
+
+  let mock_web3 = {
+    confidential: {
+      getPublicKey: (addr, cb) => {
+        cb(null, {
+          public_key: 'stkey',
+          timestamp: new Date()
+        });
+      }
+    }
+  };
+
+  it('manages a sane state machine for key progression', async function() {
+    let km = new keymanager(mock_web3, undefined, MraeBox);
+
+    // contracts start unregistered.
+    assert.equal(km.isRegistered('that'), false);
+
+    // Attempts to get local keys should fail
+    assert.throws(() => {
+      km.isRegistered('me')
+    });
+
+    km.add('that', 'magic');
+    assert.equal(km.isRegistered('that'), true);
+  
+    // re-adding the same value should be idempotent
+    km.add('that', 'magic');
+
+    assert.equal(km.isRegistered('that'), true);
+
+    // Attempts to change longterm key should fail
+    assert.throws(() => {
+      km.add('that', 'other');
+    });
+
+    // Attempts to change local key should fail
+    assert.throws(() => {
+      km.add('me', 'other');
+    });
+
+    // Load shorterm key from web3
+    let ret = null;
+    km.get('that', (key) => {ret = key;});
+    assert.equal(ret, 'stkey');
+
+    // reloading should work
+    ret = null;
+    km.get('that', (key) => {ret = key;});
+    assert.equal(ret, 'stkey');
+
+    // trying to get local key should fail.
+    ret = null;
+    assert.throws(() => {
+      km.get('me', (key) => {ret = key;});
+    })
+    assert.equal(ret, null);
+
+    km.reset();
+    assert.equal(km.isRegistered('that'), false);
+  });
 });
