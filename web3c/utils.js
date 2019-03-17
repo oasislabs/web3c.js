8de806fecc3ee49d51dfbb8ee76412c2bebd5f7e
Armani Ferrante <armaniferrante@gmail.com>
2019-03-17 13:08:46 -0700

    Apply suggestions from code review

--- web3c/utils.js
+++ web3c/utils.js
@@ -2,12 +2,12 @@
  * objectAssign is an alternative to Object.assign to work with react-native.
  * Assigns all properties from b onto a as readonly by default.
  */
-function objectAssign(a, b, enumerable = true, writeable = false) {
+function objectAssign(a, b, enumerable = true, writable = false) {
   Object.keys(b).forEach((key) => {
     Object.defineProperty(a, key, {
       value: b[key],
       enumerable,
-      writeable
+      writable
     });
   });
 }
