dd88222193e23a881f60d62bef863438502da724
Armani Ferrante <armaniferrante@gmail.com>
2019-03-17 13:07:44 -0700

    Update utils.js

--- web3c/utils.js
+++ web3c/utils.js
@@ -1,12 +1,13 @@
 /**
  * objectAssign is an alternative to Object.assign to work with react-native.
- * Assigns all properties from b onto a.
+ * Assigns all properties from b onto a as readonly by default.
  */
-function objectAssign(a, b, enumerable = true) {
+function objectAssign(a, b, enumerable = true, writeable = false) {
   Object.keys(b).forEach((key) => {
     Object.defineProperty(a, key, {
       value: b[key],
       enumerable,
+      writeable
     });
   });
 }
